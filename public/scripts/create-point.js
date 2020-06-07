009515a8037b80abb26200efa6e2963aca2deb31
Weder Pedro <weder.ssp@gmail.com>
2020-06-06 22:44:32 -0300

    fist commit

---
+++ public/scripts/create-point.js
@@ -0,0 +1,98 @@
+function popularUfs() {
+    const ufSelect = document.querySelector("select[name=uf]")
+
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+        .then((res) => { return res.json() })
+        .then(states => {
+
+            for (const state of states) {
+                ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`;
+            }
+
+        })
+}
+
+popularUfs()
+
+function getCities(event) {
+    const citySelect = document.querySelector("[name=city]")
+    const stateInput = document.querySelector("[name=state]")
+
+    const ufValue = event.target.value
+
+    const indexOfSelectedState = event.target.selectedIndex
+    stateInput.value = event.target.options[indexOfSelectedState].text
+
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+
+    citySelect.innerHTML = `<option value="">Selecione a Cidade</option>`;
+    citySelect.disabled = false;
+
+    fetch(url)
+        .then((res) => { return res.json() })
+        .then(cities => {
+
+
+            for (const city of cities) {
+                citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
+            }
+
+            citySelect.disabled = false;
+        })
+}
+
+
+document
+    .querySelector("select[name=uf]")
+    .addEventListener("change", getCities)
+
+// itens de coleta
+//pegar todos li's
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+for (const item of itemsToCollect) {
+    item.addEventListener("click", handleSelectedItem);
+}
+
+
+const collectedItems = document.querySelector("input[name=items]")
+
+let selectedItems = []
+
+function handleSelectedItem(event) {
+    const itemLi = event.target
+
+    //adicionar ou remover uma classe com javascript
+    itemLi.classList.toggle("selected");
+
+    const itemId = itemLi.dataset.id
+
+    console.log("Item ID", itemId)
+
+    // verificar se existem itens selecionados
+    // se sim, pegar itens selecionados
+
+    const alreadySelected = selectedItems.findIndex(item => {
+        const itemFound = item == itemId
+        return itemFound
+    })
+
+    // se já tiver selecionado tirar da seleção.
+    if (alreadySelected >= 0) {
+        //tirar da seleção
+        const filteredItems = selectedItems.filter(item => {
+            itemIsDifferent = item != itemId
+            return itemIsDifferent
+        })
+        selectedItems = filteredItems
+    } else {
+        // se não estiver selecionado
+        // adicionar à seleção.
+        selectedItems.push(itemId)
+    }
+
+    console.log("Items Selecionados", selectedItems)
+
+    // atualizar o campo escondido com os itens selecionados
+    collectedItems.value = selectedItems
+
+}
---
+++ src/server.js
@@ -0,0 +1,168 @@
+const express = require("express")
+const server = express()
+
+//pegar o banco de dados
+const db = require("./database/db")
+
+//configurar pasta publica
+server.use(express.static("public"))
+
+//habilitar o uso do req.body na nossa aplicação
+server.use(express.urlencoded({ extended: true }))
+
+
+//utilizando template engine
+
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+// configurar caminhos da aplicação
+// pagina inicial
+// req: Requisição
+// res: Resposta
+server.get("/", (req, res) => {
+    return res.render("index.html", { title: "Um título" })
+})
+
+server.get("/create-point", (req, res) => {
+
+    // req.query são os Query Strings da nossa Url
+    console.log(req.query)
+
+    return res.render("create-point.html")
+})
+
+server.post("/savepoint", (req, res) => {
+
+    //req.body: O corpo do bosso formulário
+    //console.log(req.body)
+
+    //inserir dados no banco de dados
+    const query = `
+        INSERT INTO places (
+            name,
+            image,
+            address,
+            address2,
+            state,
+            city,
+            items
+        ) VALUES (?,?,?,?,?,?,?);
+    `
+
+    const values = [
+        req.body.name,
+        req.body.image,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items,
+    ]
+
+    function afterInserData(err) {
+        if (err) {
+            console.log(err)
+            return res.send("Erro no Cadastro")
+        }
+
+        console.log("Cadastrado com sucesso")
+        console.log(this)
+
+        return res.render("create-point.html", { saved: true })
+    }
+
+    db.run(query, values, afterInserData)
+
+
+})
+
+server.get("/search", (req, res) => {
+
+    const searchCity = req.query.searchCity
+    const searchState = req.query.searchState
+
+    if (searchCity == "" & searchState == "") {
+        // pesquisa vazia
+        return res.render("search-results.html", { total: 0 })
+
+    } else if (searchCity != "" & searchState == "") {
+        db.all(`SELECT * FROM places where city LIKE '%${searchCity}%'`, function(err, rows) {
+            if (err) {
+                return console.log(err)
+            }
+
+            const total = rows.length
+                //   if (total <= 1) {
+
+            //       const totalPoints = total + " ponto"
+
+            //       //mostrar as páginas html com os bancos de dados
+            //       return res.render("search-results.html", { places: rows, totalPoints })
+            //   } else {
+
+            //       const totalPoints = total + " pontos"
+
+            //       //mostrar as páginas html com os bancos de dados
+            //       return res.render("search-results.html", { places: rows, totalPoints: totalPoints })
+            //   }
+
+            //mostrar as páginas html com os bancos de dados
+            return res.render("search-results.html", { places: rows, total })
+        })
+    } else if (searchCity == "" & searchState != "") {
+        db.all(`SELECT * FROM places where state LIKE '%${searchState}%'`, function(err, rows) {
+            if (err) {
+                return console.log(err)
+            }
+
+            const total = rows.length
+                //   if (total <= 1) {
+
+            //       const totalPoints = total + " ponto"
+
+            //       //mostrar as páginas html com os bancos de dados
+            //       return res.render("search-results.html", { places: rows, totalPoints })
+            //   } else {
+
+            //       const totalPoints = total + " pontos"
+
+            //       //mostrar as páginas html com os bancos de dados
+            //       return res.render("search-results.html", { places: rows, totalPoints: totalPoints })
+            //   }
+
+            //mostrar as páginas html com os bancos de dados
+            return res.render("search-results.html", { places: rows, total })
+        })
+    } else {
+        db.all(`SELECT * FROM places where city LIKE '%${searchCity}%' and state LIKE '%${searchState}%'`, function(err, rows) {
+            if (err) {
+                return console.log(err)
+            }
+
+            const total = rows.length
+                //   if (total <= 1) {
+
+            //       const totalPoints = total + " ponto"
+
+            //       //mostrar as páginas html com os bancos de dados
+            //       return res.render("search-results.html", { places: rows, totalPoints })
+            //   } else {
+
+            //       const totalPoints = total + " pontos"
+
+            //       //mostrar as páginas html com os bancos de dados
+            //       return res.render("search-results.html", { places: rows, totalPoints: totalPoints })
+            //   }
+
+            //mostrar as páginas html com os bancos de dados
+            return res.render("search-results.html", { places: rows, total })
+        })
+    }
+})
+
+//ligar o servidor
+server.listen(3000)
